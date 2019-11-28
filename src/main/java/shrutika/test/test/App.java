89a72570dca9db55c9c7827e8b477ecffdedcebb
Yadav <Shrutika_Yadav@spe.sony.com>
2019-11-28 11:04:08 +0530

    initial commit

---
+++ src/main/java/shrutika/test/test/App.java
@@ -0,0 +1,20 @@
+package shrutika.test.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static void sample(int a, int b, int c, int d, int e , int f){
+    	System.out.println("concompiant");
+    }
+    public static int sample(int a){
+    	return a;
+    }
+}
---
+++ src/test/java/shrutika/test/test/AppTest.java
@@ -0,0 +1,20 @@
+package shrutika.test.test;
+
+import org.junit.jupiter.api.Test;
+import shrutika.test.test.App;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	assertEquals( 8,App.sample(8) );
+    }
+}
